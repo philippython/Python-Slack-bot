8aa93b2a888f5f810260d403a40223b2b0ae68ad
odulaja philip <odulajaphilip@gmail.com>
2022-09-26 01:33:49 +0100

    added siging secret token

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ file for bot creation
 import os
 from datetime import datetime
 from pathlib import Path
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -14,7 +16,10 @@ CHANNELS = ['reactjs','java','sql','powerbi', 'projects','random', 'support','da
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+slack_event_adapter = SlackEventAdapter()
 client = WebClient(token=os.environ.get('SLACK_TOKEN'))
+app = Flask(__name__)
+
 
 # sending messages
 def send_message_to_channels(message, channels):
@@ -27,3 +32,5 @@ def send_message_to_channels(message, channels):
 
 
 #  handling events
+if __name__ == "__main__":
+    app.run(debug=True)
